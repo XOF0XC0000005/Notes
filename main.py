da48fd73d143a366f2a73a18d8a28991f2722f6b
XOF0XC0000005 <leva95reva@yahoo.com>
2023-12-15 09:54:47 +0300

    Handler additional
    add save and add functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import dataclasses
 import json
+import uuid
 
 @dataclasses.dataclass
 class Note:
@@ -20,3 +21,12 @@ class Handler:
                 self.notes = json.load(f)
         except:
             self.notes = dict()
+
+    def save(self):
+        with open(self.filename, 'w') as f:
+            f.write(json.dumps(self.notes, indent=4))
+
+    def add(self, note):
+        print(note)
+        print(dataclasses.asdict(note))
+        self.notes[str(uuid.uuid4())] = dataclasses.asdict(note)

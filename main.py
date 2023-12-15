cd3e3c00503b5435e5984b34fa4a8f14337b74c9
XOF0XC0000005 <leva95reva@yahoo.com>
2023-12-15 12:12:20 +0300

    Main function
    add main function and add show note function in Handler class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dataclasses
 import json
 import uuid
+import pprint
 from datetime import datetime
 
 @dataclasses.dataclass
@@ -28,15 +29,56 @@ class Handler:
             f.write(json.dumps(self.notes, indent=4))
 
     def add(self, note):
-        print(note)
-        print(dataclasses.asdict(note))
+        #print(note)
+        #print(dataclasses.asdict(note))
         self.notes[str(uuid.uuid4())] = dataclasses.asdict(note)
-        print(self.notes)
 
     def delete(self, id: str):
         self.notes.pop(id)
 
     def update(self, id, title, msg):
-        self.notes['id']['title'] = title if title else title
-        self.notes['id']['msg'] = msg if msg else msg
-        self.notes['id']['date'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        self.notes[id]['title'] = title if title else title
+        self.notes[id]['msg'] = msg if msg else msg
+        self.notes[id]['date'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+    def show(self, id):
+        note = self.notes.get(id)
+        pprint.pprint(note)
+
+    def showAll(self, isFiltered):
+        if (isFiltered == "-"):
+            pprint.pprint(self.notes)
+        elif (isFiltered == "+"):
+            dateFilter = input("Введите дату для фильтрации в формате dd/mm/yyyy: ")
+            for k, v in self.notes.items():
+                note = str(self.notes.get(k)["date"]).split(", ")
+                if note[0] == dateFilter:
+                    pprint.pprint(self.notes.get(k))
+
+if __name__ == '__main__':
+    a = Handler('notes.json')
+    a.get()
+    print('Для выхода введите команду - exit')
+    while True:
+        command = input("Введите команду: ")
+        if command == "add":
+            title = input("Введите название: ")
+            msg = input("Введите тело: ")
+            a.add(Note(title=title, msg=msg, date=datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
+        elif command == "update":
+            id = input('Введите индентификатор: ')
+            title = input("Введите название: ")
+            msg = input("Введите тело: ")
+            a.update(id, title, msg)
+        elif command == "delete":
+            id = input("Введите индентификатор: ")
+            a.delete(id)
+        elif command == "show":
+            id = input("Введите индентификатор: ")
+            a.show(id)
+        elif command == "show all":
+            isFiltered = input("Необходим фильтр по дате(Введите + или -): ")
+            a.showAll(isFiltered)
+        elif command == "exit":
+            break
+        a.save()

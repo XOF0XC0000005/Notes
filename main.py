193487a1dc45cb364ecfe06d2f23e8a0bc530d80
XOF0XC0000005 <leva95reva@yahoo.com>
2023-12-15 09:50:15 +0300

    Handler
    create class handler and get function

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import dataclasses
+import json
 
 @dataclasses.dataclass
 class Note:
     title: str
     msg: str
     date: str
+
+class Handler:
+
+    notes = dict()
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get(self):
+        try:
+            with open(self.filename, 'r') as f:
+                self.notes = json.load(f)
+        except:
+            self.notes = dict()
